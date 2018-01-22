b82720ba2f9fababd3e3b664bc93b8ba391e0381
Bjorn <bjossijohnson@gmail.com>
2018-01-22 15:58:30 +0100

    map componenter created

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,11 +1,12 @@
 import React from "react"
-
+import Map from "./map"
 class App extends React.Component {
 
   render() {
     return (
       <div>
         System Club!
+        <Map />
       </div>
     )
   }
---
+++ frontend/src/components/map/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./index.css"
+
+export default class Map extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Prufa
+      </div>
+    )
+  }
+}
