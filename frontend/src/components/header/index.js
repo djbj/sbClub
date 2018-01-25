cf9b973aaca950d24ffc563531fbb1abe273e288
Bjorn <bjossijohnson@gmail.com>
2018-01-25 11:05:51 +0100

    working

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -3,10 +3,6 @@ import "./index.css"
 
 class Header extends React.Component {
 
-//   navigator.geolocation.getCurrentPosition(function(position) {
-//   console.log(position.coords.latitude, position.coords.longitude);
-// });
-
   render() {
     return (
       <div className="header-container">
--- frontend/src/components/map/index.js
+++ frontend/src/components/map/index.js
@@ -4,24 +4,38 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker, Rectangle } from "react
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    // myApiKey = "AIzaSyBEDZiGba8Eukfh-eDXzlAES3IS-Fh3qVc",
+    // googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBEDZiGba8Eukfh-eDXzlAES3IS-Fh3qVc&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }), withScriptjs, withGoogleMap)((props) =>
   <GoogleMap
     defaultZoom={14}
-    defaultCenter={{ lat: 59.3170826, lng: 18.0275315}}
+    // defaultCenter={{ lat: 59.3170826, lng: 18.0275315}}
+    defaultCenter={{ lat: props.lat, lng: props.lng}}
     clickableIcons={false}
   >
-    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} onClick={props.onMarkerClick} />}
+    {props.isMarkerShown && <Marker position={{ lat: 59.317082, lng: 18.0275315 }} onClick={props.onMarkerClick} />}
   </GoogleMap>
-);
+)
 
 export default class Map extends React.PureComponent {
   state = {
     isMarkerShown: false,
   }
+  mapSuccess = (pos) => {
+    const crd = pos.coords
+    console.log('Your current position is:')
+    console.log(`Latitude : ${crd.latitude}`)
+    console.log(`Longitude: ${crd.longitude}`)
+    console.log(`More or less ${crd.accuracy} meters.`)
+  }
+
+  mapError = (err) => {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  };
 
   componentDidMount() {
     this.delayedShowMarker()
@@ -39,8 +53,14 @@ export default class Map extends React.PureComponent {
   }
 
   render() {
+    navigator.geolocation.getCurrentPosition(this.mapSuccess, this.mapError)
+      // navigator.geolocation.getCurrentPosition(this.mapSuccess, this.mapError, this.options)
+      // if ("geolocation" in navigator) {console.log("geolocation is available")}
+      // else {console.log("gelocation is not available")}
     return (
       <MyMapComponent
+        lat={59.3170826}
+        lng={18.0275315}
         isMarkerShown={this.state.isMarkerShown}
         onMarkerClick={this.handleMarkerClick}
       />
