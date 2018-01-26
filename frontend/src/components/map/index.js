d071ae8b81df2a611e66de0666f24aefb6b8cdff
Bjorn <bjossijohnson@gmail.com>
2018-01-26 17:47:14 +0100

    bara sma

--- frontend/src/components/map/index.js
+++ frontend/src/components/map/index.js
@@ -23,12 +23,14 @@ const MyMapComponent = compose(withProps({
       onClick={props.onMarkerClick} />}
   </GoogleMap>)
 
-const MapWithAMarker = withGoogleMap( props =>
+const MapWithAMarker = withGoogleMap(props =>
   <GoogleMap
     defaultZoom={8}
     defaultCenter={{ lat: -34.397, lng: 150.644 }}>
     <Marker
       position={{ lat: -34.397, lng: 150.644 }} />
+    <Marker
+      position={{ lat: -35, lng: 150 }} />
   </GoogleMap>)
 
 export default class Map extends React.PureComponent {
@@ -88,10 +90,9 @@ export default class Map extends React.PureComponent {
           lng={parseFloat(this.props.appLng)}
           isMarkerShown={this.state.isMarkerShown}
           onMarkerClick={this.handleMarkerClick} />
-          <MapWithAMarker
-              containerElement={<div style={{ height: `400px` }} />}
-              mapElement={<div style={{ height: `100%` }} />}
-          />
+        <MapWithAMarker
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />} />
       </div>
     )
   }
