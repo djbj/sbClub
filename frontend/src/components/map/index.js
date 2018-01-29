65926f434f732299eec0a80e42e3df68134cfa27
Bjorn <bjossijohnson@gmail.com>
2018-01-29 23:54:37 +0100

    comment out

--- frontend/src/components/map/index.js
+++ frontend/src/components/map/index.js
@@ -82,41 +82,41 @@ const MyMapComponent = compose(
       {props.directions && <DirectionsRenderer directions={props.directions} />}
   </GoogleMap>)
 
-const MapWithADirectionsRenderer = compose(withProps({
-  // googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBEDZiGba8Eukfh-eDXzlAES3IS-Fh3qVc&v=3.exp&libraries=geometry,drawing,places",
-  googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBEDZiGba8Eukfh-eDXzlAES3IS-Fh3qVc&v=3.exp&libraries=geometry,drawing,places",
-  loadingElement: <div style={{ height: "100%" }} />,
-  containerElement: <div style={{ height: "400px" }} />,
-  mapElement: <div style={{ height: "100%" }} />
-}), withScriptjs, withGoogleMap,
-lifecycle({
-  componentDidMount() {
-    const DirectionsService = new google.maps.DirectionsService()
-    // const DistanceService = new google.maps.DistanceMatrixService()
-    console.log(this.props.originLat, this.props.originLng)
-    DirectionsService.route({
-      origin: new google.maps.LatLng(41.8507300, -87.6512600),
-      // origin: new google.maps.LatLng(this.props.originLat, this.props.originLng),
-      destination: new google.maps.LatLng(41.8525800, -87.6514100),
-      travelMode: google.maps.TravelMode.WALKING
-    }, (result, status) => {
-      if (status === google.maps.DirectionsStatus.OK) {
-        console.log(`Results ${result}`)
-        this.setState({
-          directions: result
-        })
-      } else {
-        console.error(`error fetching directions ${result}`)
-      }
-    })
-  }
-})
-)(props =>
-  <GoogleMap
-    defaultZoom={7}
-    defaultCenter={new google.maps.LatLng(41.8507300, -87.6512600)}>
-    {props.directions && <DirectionsRenderer directions={props.directions} />}
-  </GoogleMap>)
+// const MapWithADirectionsRenderer = compose(withProps({
+//   // googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBEDZiGba8Eukfh-eDXzlAES3IS-Fh3qVc&v=3.exp&libraries=geometry,drawing,places",
+//   googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBEDZiGba8Eukfh-eDXzlAES3IS-Fh3qVc&v=3.exp&libraries=geometry,drawing,places",
+//   loadingElement: <div style={{ height: "100%" }} />,
+//   containerElement: <div style={{ height: "400px" }} />,
+//   mapElement: <div style={{ height: "100%" }} />
+// }), withScriptjs, withGoogleMap,
+// lifecycle({
+//   componentDidMount() {
+//     const DirectionsService = new google.maps.DirectionsService()
+//     // const DistanceService = new google.maps.DistanceMatrixService()
+//     console.log(this.props.originLat, this.props.originLng)
+//     DirectionsService.route({
+//       origin: new google.maps.LatLng(41.8507300, -87.6512600),
+//       // origin: new google.maps.LatLng(this.props.originLat, this.props.originLng),
+//       destination: new google.maps.LatLng(41.8525800, -87.6514100),
+//       travelMode: google.maps.TravelMode.WALKING
+//     }, (result, status) => {
+//       if (status === google.maps.DirectionsStatus.OK) {
+//         console.log(`Results ${result}`)
+//         this.setState({
+//           directions: result
+//         })
+//       } else {
+//         console.error(`error fetching directions ${result}`)
+//       }
+//     })
+//   }
+// })
+// )(props =>
+//   <GoogleMap
+//     defaultZoom={7}
+//     defaultCenter={new google.maps.LatLng(41.8507300, -87.6512600)}>
+//     {props.directions && <DirectionsRenderer directions={props.directions} />}
+//   </GoogleMap>)
 
 const MapWithAMarker = withGoogleMap(props =>
   <GoogleMap
