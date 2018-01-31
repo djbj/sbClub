1a66247d89e4d385dae0fca1afd0c3ce9be3b4c2
Bjorn <bjossijohnson@gmail.com>
2018-01-31 23:14:07 +0100

    opening times

--- frontend/src/components/store/index.js
+++ frontend/src/components/store/index.js
@@ -6,11 +6,33 @@ class Store extends React.Component {
     super(props)
     this.state = {
       isStoreChosen: false,
-      // openHrs: ""
+      openUntil: "19:00"
     }
   }
 
-  componentDidMount() {
+  // componentDidMount() {
+  //   const today = new Date()
+  //   let dd = today.getDate()
+  //   let mm = today.getMonth() + 1 // January is 0!
+  //   const yyyy = today.getFullYear()
+  //   // const currentTime = today.getHours() + ":" + today.getMinutes()
+  //   // const currentTime = `${today.getHours()}:${today.getMinutes()}`
+  //   // console.log(currentTime)
+  //   if (dd < 10) { dd = `0${dd}` }
+  //   if (mm < 10) { mm = `0${mm}` }
+  //   const todayDate = `${yyyy}-${mm}-${dd}`
+  //   // if(openHrs) { console.log("openHrs" + openHrs[0][0]) }
+  //   // const openHrs.map(day => console.log("This is openingday" + day))
+  //   // console.log("lengd"+openHrs.length)
+  //   const openHrs = this.props.openingHrs
+  //   console.log("OpeningHrsLength" + openHrs.length)
+  //
+  //   // for (var i = 0; i < openHrs.length; i++) {
+  //   //   console.log("inArrey")
+  //   // }
+  // }
+
+  getOpeningTimes = openHrs => {
     const today = new Date()
     let dd = today.getDate()
     let mm = today.getMonth() + 1 // January is 0!
@@ -23,32 +45,11 @@ class Store extends React.Component {
     const todayDate = `${yyyy}-${mm}-${dd}`
     // if(openHrs) { console.log("openHrs" + openHrs[0][0]) }
     // const openHrs.map(day => console.log("This is openingday" + day))
-    // console.log("lengd"+openHrs.length)
-    const openHrs = this.props.openingHrs
-    console.log("OpeningHrsLength" + openHrs.length)
+    console.log("lengd"+openHrs.length)
     // for (var i = 0; i < openHrs.length; i++) {
     //   console.log("inArrey")
     // }
   }
-
-//   getOpeningTimes = openHrs => {
-//     const today = new Date()
-//     let dd = today.getDate()
-//     let mm = today.getMonth() + 1 // January is 0!
-//     const yyyy = today.getFullYear()
-//     // const currentTime = today.getHours() + ":" + today.getMinutes()
-//     // const currentTime = `${today.getHours()}:${today.getMinutes()}`
-//     // console.log(currentTime)
-//     if (dd < 10) { dd = `0${dd}` }
-//     if (mm < 10) { mm = `0${mm}` }
-//     const todayDate = `${yyyy}-${mm}-${dd}`
-//     // if(openHrs) { console.log("openHrs" + openHrs[0][0]) }
-//     // const openHrs.map(day => console.log("This is openingday" + day))
-//     // console.log("lengd"+openHrs.length)
-//     for (var i = 0; i < openHrs.length; i++) {
-//       console.log("inArrey")
-//     }
-// }
 
   handleClick = () => {
     console.clear()
@@ -79,7 +80,7 @@ class Store extends React.Component {
             {/* <div className="store-nr"> {this.props.nr} </div> */}
             <span className="store-address">{this.props.name} {this.props.address1}</span>
             <span className="store-coords">Lat: {this.props.storeLat} Lng: {this.props.storeLng} </span>
-            <span className="store-hrs">Open Today Until: {this.props.openingHrs}</span>
+            <span className="store-hrs">Open Today Until: {this.state.openUntil}</span>
           </div>
         </a>
       </div>
