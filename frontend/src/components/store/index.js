e704d06973fa63c2a01885f74871105363cbab73
Bjorn <bjossijohnson@gmail.com>
2018-02-01 10:41:52 +0100

    cond rendering of opening hrs and closed

--- frontend/src/components/store/index.js
+++ frontend/src/components/store/index.js
@@ -6,15 +6,23 @@ class Store extends React.Component {
     super(props)
     this.state = {
       isStoreChosen: false,
-      openingTimes: this.props.openingHrs,
-      openFrom: "",
-      openUntil: "",
+      openingTimes: "",
       timeToClose: "",
       travelTime: ""
     }
   }
 
+  componentWillMount() {
+    const setOpeningTimes = this.getOpeningTimes(this.props.openingHrs)
+    // const setTimeToClose = this.getTimeToClose()
+    this.setState({
+      openingTimes: setOpeningTimes,
+      // timeToClose: setTimeToClose
+    })
+  }
+
   getOpeningTimes = openingTimes => {
+    console.log("openingtimes")
     const today = new Date()
     let dd = today.getDate()
     let mm = today.getMonth() + 1 // January is 0!
@@ -25,33 +33,20 @@ class Store extends React.Component {
     if (dd < 10) { dd = `0${dd}` }
     if (mm < 10) { mm = `0${mm}` }
     const todayDate = `${yyyy}-${mm}-${dd}`
-    // if(openHrs) { console.log("openHrs" + openHrs[0][0]) }
-    // openingTimes.map(day => console.log("This is openingday" + day))
-    // console.log("lengd" + openingTimes.length)
-    // openingTimes.forEach((open)=>{ console.log(open) })
-    // let i = 0;
     // for (let times of openingTimes) {
     for (let i = 0; i < openingTimes.length; i++) {
       if (todayDate.localeCompare(openingTimes[i][0]) === 0) {
       // if (todayDate === openingTimes[i][0]) {
-        console.log("Open until:" + openingTimes[i][2])}
-        return openingTimes[i][2]
-      // this.setState({
-      //   openFrom: openingTimes[i][1],
-      //   openUntil: openingTimes[i][2]
-      // })
-      //   return openingTimes[i][2]
-      // } else {
-      //   return "Could not find opening times"
-      // }
-      // }
+        console.log("opening " + openingTimes[i][1])
+        return openingTimes[i]
+      }
     }
   }
 
   getTimeToClose = () => {
     const d = new Date()
     // let closingTime = this.state.timeToClose
-    let closingTime = this.state.openUntil
+    let closingTime = this.state.openingTimes[2]
     closingTime = closingTime.split(":")
     const minutesToClosing = ((-d + d.setHours(closingTime[0], closingTime[1], 0, 0)) / 6e4)
     this.setState({
@@ -76,10 +71,9 @@ class Store extends React.Component {
   }
 
   render() {
-    // console.log("Rendering Store")
-    // console.log(this.props.openingHrs)
     // this.getTimeToClose()
-    const openTodayUntil = this.getOpeningTimes(this.props.openingHrs)
+    const openToday = this.getOpeningTimes(this.props.openingHrs)
+    // const openToday = ["","","00:00"]
     return (
       <div className="store" onClick={this.handleClick}>
         <a href="#">
@@ -87,10 +81,18 @@ class Store extends React.Component {
             <div className="store-name"><span className="systemet">Club</span></div>
             {/* <div className="store-nr"> {this.props.nr} </div> */}
             <span className="store-address">{this.props.name} {this.props.address1} </span>
-            <span className="store-coords">Lat: {this.props.storeLat} Lng: {this.props.storeLng} </span>
-            <span className="store-hrs">Open Today Until: {openTodayUntil} </span>
+            {/* <span className="store-coords">Lat: {this.props.storeLat} Lng: {this.props.storeLng} </span> */}
+            {openToday[2]==="00:00" ? (
+              <span className="store-hrs">Closed Today</span>
+            ) : (
+              <div>
+                <span className="store-hrs">Open Today Until: {openToday[2]} </span>
+                <span className="closes-in">Closes in: {this.props.timeToClose} </span>
+              </div>
+            )}
+            {/* <span className="store-hrs">Open Today Until: {openToday[2]} </span> */}
             {/* <span className="closes-in">Closes in: {this.state.timeToClose} </span> */}
-            <span className="closes-in">Closes in: {this.props.timeToClose} </span>
+            {/* <span className="closes-in">Closes in: {this.props.timeToClose} </span> */}
             <span className="walking-time">Travel time: {this.state.travelTime} </span>
           </div>
         </a>
